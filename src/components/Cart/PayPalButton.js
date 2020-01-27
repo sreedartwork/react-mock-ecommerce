b37954357ffb73cd067b7a590d033261a31381dc
Sean Reed <sreedartwork@gmail.com>
2020-01-27 15:48:38 -0500

    changed paypal to test button

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -30,7 +30,9 @@ export default class MyApp extends React.Component {
     // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
 
     const client = {
-      sandbox: process.env.REACT_APP_APP_ID,
+      sandbox:
+        AWHSHd89pLvyeoDHEzMIZmpsLL50oVfRN5AWTO1UC4wXg_L4F -
+        jMPcYACNya7VRKavgII9fMSOHJo8E6,
       production: "YOUR-PRODUCTION-APP-ID"
     };
     // In order to get production's app-ID, you will have to send your app to Paypal for approval first
