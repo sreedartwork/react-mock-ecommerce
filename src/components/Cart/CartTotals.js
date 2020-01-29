e00effef65194f097b3ffd2ca2d40dc64fa0560a
Sean Reed <sreedartwork@gmail.com>
2020-01-29 14:26:09 -0500

    removed  paypal button

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import PayPalButton from "./PayPalButton";
+// import PayPalButton from "./PayPalButton";
 export default function CartTotals({ value, history }) {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
   return (
@@ -29,11 +29,11 @@ export default function CartTotals({ value, history }) {
               <span className="text-title">total :</span>
               <strong>$ {cartTotal}</strong>
             </h5>
-            <PayPalButton
+            {/* <PayPalButton
               total={cartTotal}
               clearCart={clearCart}
               history={history}
-            />
+            /> */}
           </div>
         </div>
       </div>
